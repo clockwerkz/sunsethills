1f0d63d09ab3f522093232e5624e9aa9aee33b5b
clockwerkz <clockwerkz@gmail.com>
2020-06-19 00:12:09 -0700

    created production build for heroku deployment

---
+++ client/build/precache-manifest.808cf2c0b1788a52155d00add67b7f1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731d55930ad6665bf8cfa89d31d576da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05964c8696dee6fe8f8f",
+    "url": "/static/css/main.5f727199.chunk.css"
+  },
+  {
+    "revision": "ac955b3f3c3adf3f96b4",
+    "url": "/static/js/2.bf576561.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf576561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05964c8696dee6fe8f8f",
+    "url": "/static/js/main.f81a4584.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ba69a26511b60cb4edaaf2dc50ad5e8d",
+    "url": "/static/media/sunset-512.ba69a265.png"
+  }
+]);
